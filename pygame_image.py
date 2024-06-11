15adceae5ac0649ec7a446f8eb8fab6e5cf74ac3
fujisan <c0b2312277@edu.teu.ac.jp>
2024-06-11 16:20:19 +0900

    add move_koukaton

--- pygame_image.py
+++ pygame_image.py
@@ -19,13 +19,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) # 背景画像の貼り付け(表示)
+        # screen.blit(bg_img, [0, 0]) # 背景画像の貼り付け(表示)
+        x=tmr%800
+        screen.blit(bg_img, [-x, 0]) # 背景画像の貼り付け(表示)
         kt1_rct = kt1_img.get_rect() # 画像の中心を抽出
         kt1_rct .center=300,200 # 中心を300,200に設定
         screen.blit(kt1_img,kt1_rct) # 画像の表示(kt1_rctの位置にする)
 
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+                
         clock.tick(200)
 
 
